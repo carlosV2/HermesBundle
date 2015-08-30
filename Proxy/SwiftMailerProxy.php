ec23dce7f29a99cc8cb9f23b4e7be8aa2c873eb0
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2015-08-31 00:17:35 +0100

    Store a copy of each email sent trough the proxy

--- Proxy/SwiftMailerProxy.php
+++ Proxy/SwiftMailerProxy.php
@@ -2,13 +2,22 @@
 
 namespace Carlosv2\HermesBundle\Proxy;
 
+use Carlosv2\HermesBundle\Repository\MessageRepository;
+
 class SwiftMailerProxy extends \Swift_Mailer
 {
     /**
+     * @var MessageRepository
+     */
+    private $repository;
+
+    /**
      * {@inheritdoc}
      */
-    public function __construct(\Swift_Transport $transport)
+    public function __construct(\Swift_Transport $transport, MessageRepository $repository)
     {
+        $this->repository = $repository;
+
         parent::__construct($transport);
     }
 
@@ -17,6 +26,8 @@ class SwiftMailerProxy extends \Swift_Mailer
      */
     public function send(\Swift_Mime_Message $message, &$failedRecipients = null)
     {
+        $this->repository->save($message);
+
         return parent::send($message, $failedRecipients);
     }
 }
