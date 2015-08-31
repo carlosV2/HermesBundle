fb38f0ade4232ee9fbeccb3a59a44e59646345a7
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2015-08-31 01:10:54 +0100

    Ensure repository path exists

--- Repository/MessageRepository.php
+++ Repository/MessageRepository.php
@@ -17,7 +17,19 @@ class MessageRepository
      */
     public function __construct($path)
     {
+        $this->ensurePathExists(dirname($path));
+
         $this->repository = new FileRepository($path, new AccessorObjectIdentifier('getId'));
+    }
+
+    /**
+     * @param string $path
+     */
+    private function ensurePathExists($path)
+    {
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
     }
 
     /**
